c2c3d57863a464dbdb754f3eee6e93b401c119d6
JeshuaNarvaezCollado <crazylobo3018@gmail.com>
2023-03-15 23:33:13 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello World!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["the", "our"];
+  let adjective = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let adjIndx = Math.floor(Math.random() * adjective.length);
+  let nounIndx = Math.floor(Math.random() * noun.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    possession[possessionIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
